cc16995f46e01990ceb4819f6c71d96152952d08
ClausiusMaximus <155851699+ClausiusMaximus@users.noreply.github.com>
2026-07-08 23:10:33 +0200

    Aktualisieren von app.js

--- js/app.js
+++ js/app.js
@@ -8,6 +8,33 @@ async function init() {
     initDatabase();
 }
 
+function setupNowButton() {
+
+    const button = document.getElementById("nowButton");
+
+    button.addEventListener("click", setCurrentDateTime);
+
+}
+
+function setCurrentDateTime() {
+
+    const now = new Date();
+
+    const date =
+        now.getFullYear() + "-" +
+        String(now.getMonth() + 1).padStart(2, "0") + "-" +
+        String(now.getDate()).padStart(2, "0");
+
+    const time =
+        String(now.getHours()).padStart(2, "0") + ":" +
+        String(now.getMinutes()).padStart(2, "0") + ":" +
+        String(now.getSeconds()).padStart(2, "0");
+
+    document.getElementById("attackDate").value = date;
+    document.getElementById("attackTime").value = time;
+
+}
+
 async function registerServiceWorker() {
 
     if (!("serviceWorker" in navigator)) {
