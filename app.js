398523d1b7795b4e76922b8f55eb9c52b9b0f2d2
ClausiusMaximus <155851699+ClausiusMaximus@users.noreply.github.com>
2026-07-08 10:09:05 +0200

    Aktualisieren von app.js
    feat(ui): create attack entry screen

--- app.js
+++ app.js
@@ -21,3 +21,27 @@ newBtn.onclick=()=>showPage("newPage");
 calendarBtn.onclick=()=>showPage("calendarPage");
 
 statsBtn.onclick=()=>showPage("statsPage");
+
+const kipGrid=document.getElementById("kipGrid");
+
+for(let i=1;i<=10;i++){
+
+    const b=document.createElement("button");
+
+    b.className="kipButton";
+
+    b.textContent=i;
+
+    b.onclick=()=>{
+
+        document
+        .querySelectorAll(".kipButton")
+        .forEach(btn=>btn.style.background="#2563eb");
+
+        b.style.background="#ef4444";
+
+    };
+
+    kipGrid.appendChild(b);
+
+}
