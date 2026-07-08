f5c622c2d0a296e5c45ef20df4a2738053a5c8b2
ClausiusMaximus <155851699+ClausiusMaximus@users.noreply.github.com>
2026-07-08 09:25:52 +0200

    app.js erstellen

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("ClusterLog gestartet.");
+
+document
+.getElementById("newBtn")
+.addEventListener("click",()=>{
+
+alert("Neue Attacke kommt im nächsten Build.");
+
+});
+
+document
+.getElementById("calendarBtn")
+.addEventListener("click",()=>{
+
+alert("Kalender kommt im nächsten Build.");
+
+});
+
+document
+.getElementById("statsBtn")
+.addEventListener("click",()=>{
+
+alert("Statistik kommt im nächsten Build.");
+
+});
